46dc0e5f4edbcd8c448f1ae51a079236518e10dc
londaploetz <lploetz@hotmail.com>
2023-01-16 18:37:06 -0600

    changes to layout

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
 export { default as About } from './about/About'; 
-export { default as Grid } from './grid/Grid';
+export { default as Grid } from './table/Grid';
 export { default as Hours } from './hours/Hours';
 export { default as Navbar } from './navbar/Navbar';
